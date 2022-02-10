62453f350780381414cd3a231d37b58232513273
Darshan Gowda R <darshangowdar@pop-os.localdomain>
2022-02-10 11:52:17 +0530

    Ability to delete the value based on its key valu or delete 40 from the list.

---
+++ LinkedList/MyNodeTestUC9.java
@@ -0,0 +1,29 @@
+package linkedList;
+
+
+public class MyNodeTest {
+
+	public static void main(String[] args) {
+
+		given3NumbersWhenLinkedShouldPassedLinkedListTest();
+
+	}
+
+
+	public static void given3NumbersWhenLinkedShouldPassedLinkedListTest(){
+
+		LinkedList link = new LinkedList();
+
+		link.append(56);
+		link.append(70);
+		link.insertBetween(56 , 70 ,30);
+		link.insertBetween(30 , 70 ,40);
+		link.delValue(40);
+		link.print();
+
+	}
+}
+
+
+
+
